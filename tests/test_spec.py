553d8d6136b79528d37f2a8998786f032f5617ca
Zdzisław Krajewski <zdzichucb@gmail.com>
2017-06-08 16:20:45 +0200

    Added test for checking api title

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -9,7 +9,7 @@ class SpecTest(TestCase):
     def setUp(self):
         super(SpecTest, self).setUp()
         self.app = Application()
-        self.api = Api(self.app, title="Api title")
+        self.api = Api(self.app, title='Api title')
         self.ns = self.api.namespace('api')
 
     def test_route_spec(self):
@@ -42,6 +42,7 @@ class SpecTest(TestCase):
                 '''
                 pass
         doc = self.api.spec.to_dict()
+        assert doc['info']['title'] == 'Api title'
         assert '/api/path' in doc['paths']
         assert doc['paths']['/api/path']['get']['description'] == \
             'Get a greeting'
