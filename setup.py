83c186a5c115161208c1098f49ad99f884398530
Zdzisław Krajewski <zdzichucb@gmail.com>
2017-06-08 16:39:42 +0200

    Added setup.py, updated requirements and .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tornado-restplus',
+    version='0.0.1dev',
+    url='https://github.com/d21d3q/tornado-restplus',
+    author='Zdzislaw Krajewski',
+    author_email='zdzislaw.krajewski@protonmail.ch',
+    packages=['tornado_restplus'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
